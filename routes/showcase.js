9ef7fe4f9a4ba03cef4623d2ce2f2e0585d0e445
Eric Torti <eric.torti@vivareal.com>
2015-09-19 09:31:40 -0300

    working showcase

--- routes/showcase.js
+++ routes/showcase.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var md5 = require('md5');
+
 var users = require('../db/users')();
 
+function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
+
+function generateHash(timestamp) {
+  var salt = "666";
+  return md5(timestamp + salt);
+}
+
 router.get('/', function(req, res) { 
-  res.render('mail-template', { title: 'The mail template!' })
+  var userId = pickRandomProperty(users);
+  var events = users[userId]
+  var timestamp = new Date().getTime();
+  res.render('mail-template', 
+    { userId: userId, 
+      propertyId: events[0]['propertyId'],
+      timestamp: timestamp,
+      hash: generateHash(timestamp)
+    })
 });
 
 module.exports = router;
