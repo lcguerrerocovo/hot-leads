ecbbcd296f06260880565b76adf29ed2e9a39dc1
Eric Torti <eric.torti@vivareal.com>
2015-09-19 02:34:56 -0300

    scaffolding api frontend call

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Hot leads' });
+router.get('/:account/:hash', function(req, res) {
+  var response = { 
+    account: req.param('account'),
+    rental: {
+      prices: [100, 200, 300, 400],
+      geo: [{}], 
+    }
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(response));
 });
 
 module.exports = router;
