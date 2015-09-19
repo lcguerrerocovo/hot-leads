bca166281c9bfa01aff2f8e97f33a625704739ef
Eric Torti <eric.torti@vivareal.com>
2015-09-19 07:56:44 -0300

    preparing path to receive parms + absolute path to static assets

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-router.get('/', function(req, res) {
+router.get('/:user/:listing/:hash/:timestamp', function(req, res) {
   res.sendFile('public/index.html', {root: './'});
 });
 
