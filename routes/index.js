b707df6ad867c3452aa57cd705788b96646f0e37
Eric Torti <eric.torti@vivareal.com>
2015-09-19 06:08:48 -0300

    simplyfing validation for now

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,54 @@
-
 var express = require('express');
 var md5 = require('md5');
 var router = express.Router();
 
-// change to secure salt later
-var salt = "666";
 
-// milliseconds until content expiration
-var expirationTime = 60000;
 
-router.get('/:account/:hash/:timestamp', function(req, res) {
+var csv = require('fast-csv');
+var fileStream = fs.createReadStream("db/leads.csv"),
+        parser = fastCsv();
 
+var validateHash = function(hash, timestamp) {
+  // change to secure salt later
+  var salt = "666";
+
+  // milliseconds until content expiration
+  var expirationTime = 60000;
+
+  var timestampHash = md5(timestamp + salt);
+  var currentTime = Date.now();
+  var secondsToLive = currentTime - timestamp;
+
+  return true; // hash == timestampHash && secondsToLive < expirationTime
+}
+
+router.get('/:user/:account/:listing/:hash/:timestamp', function(req, res) {
+
+    var user = req.param('user');
     var account = req.param('account');
-    var timestamp = req.param('timestamp');
+    var listing = req.param('listing');
     var hash = req.param('hash');
+    var timestamp = req.param('timestamp');
 
-    var timestampHash = md5(timestamp + salt);
-    var currentTime = Date.now();
-    var secondsToLive = currentTime - timestamp;
+    if(validateHash(hash, timestamp)) {
+      var response = {
+          account: account,
+          timestamp: timestamp,
+          hash: timestampHash,
+          currentTime: currentTime,
+          secondsToLive: secondsToLive,
 
-	// commenting validation and time expiration during development
+          rental: {
+              prices: [100, 200, 300, 400],
+              geo: [{}],
+          }
+      };
 
-	// hash and timestamp + salt have to coincide
-    if (true/*timestampHash == hash*/) {
-    	// content expires after expirationTime
-        if (true/*secondsToLive < expirationTime*/) {
-            var response = {
-                account: account,
-                timestamp: timestamp,
-                hash: timestampHash,
-                currentTime: currentTime,
-                secondsToLive: secondsToLive,
-
-                rental: {
-                    prices: [100, 200, 300, 400],
-                    geo: [{}],
-                }
-            };
-
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(response));
-        } else {
- 			res.status(422);
-        	res.send('Content expired');
-        }
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(response));
     } else {
-        res.status(400);
-        res.send('Invalid Request');
+      res.status(400);
+      res.send('Bad request');
     }
 });
 
