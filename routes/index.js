992b7388c95cd0d1c70fc56569fb6e6896e69b44
Luis Guerrero <lguerrero@vivareal.com>
2015-09-19 03:34:38 -0300

    adding secure url (hash needs to match with md5 of timestamp + salt) with time expiration

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,53 @@
+
 var express = require('express');
+var md5 = require('md5');
 var router = express.Router();
 
-router.get('/:account/:hash', function(req, res) {
-  var response = { 
-    account: req.param('account'),
-    rental: {
-      prices: [100, 200, 300, 400],
-      geo: [{}], 
-    }
-  };
+// change to secure salt later
+var salt = "666";
 
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(response));
+// milliseconds until content expiration
+var expirationTime = 60000;
+
+router.get('/:account/:hash/:timestamp', function(req, res) {
+
+    var account = req.param('account');
+    var timestamp = req.param('timestamp');
+    var hash = req.param('hash');
+
+    var timestampHash = md5(timestamp + salt);
+    var currentTime = Date.now();
+    var secondsToLive = currentTime - timestamp;
+
+	// commenting validation and time expiration during development
+
+	// hash and timestamp + salt have to coincide
+    if (true/*timestampHash == hash*/) {
+    	// content expires after expirationTime
+        if (true/*secondsToLive < expirationTime*/) {
+            var response = {
+                account: account,
+                timestamp: timestamp,
+                hash: timestampHash,
+                currentTime: currentTime,
+                secondsToLive: secondsToLive,
+
+                rental: {
+                    prices: [100, 200, 300, 400],
+                    geo: [{}],
+                }
+            };
+
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(response));
+        } else {
+ 			res.status(422);
+        	res.send('Content expired');
+        }
+    } else {
+        res.status(400);
+        res.send('Invalid Request');
+    }
 });
 
 module.exports = router;
