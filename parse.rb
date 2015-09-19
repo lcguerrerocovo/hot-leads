ed77fa57621959638793c8ee599efdc685145431
Eric Torti <eric.torti@vivareal.com>
2015-09-19 03:26:29 -0300

    data extracting

--- parse.rb
+++ parse.rb
@@ -4,5 +4,5 @@ puts "userId,timestamp,accountId,propertyId,business,salePrice,rentPrice"
 ARGF.each do |event|
   e = JSON.parse(event)
   a = e['event']['attributes']
-  puts "#{a['userId']};#{e['ct']};#{a['accountId']};#{a['propertyId']};#{a['business']};#{a['salePrice']};#{a['rentPrice']}"
+  puts "#{a['userId']},#{e['ct']},#{a['accountId']},#{a['propertyId']},#{a['business']},#{a['salePrice']},#{a['rentPrice']}"
 end
